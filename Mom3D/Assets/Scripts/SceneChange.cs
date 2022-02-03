c4ca52e01a3a251c5922ad3762e96d117bb581fc
Naomi Kaufman <accounts@warducks.com>
2022-02-03 19:53:31 +0000

    Explode

--- Mom3D/Assets/Scripts/SceneChange.cs
+++ Mom3D/Assets/Scripts/SceneChange.cs
@@ -6,21 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public float timer = 5;
 
     int nextSceneIndex;
+
+    public GameObject toTurnOn;
+    public MeshRenderer visual;
 
     public void Awake()
     {
         nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerStay(Collider other)
     {
         if (other.tag == "Object")
         {
+            visual.enabled = false;
+            toTurnOn.SetActive(true);
 
-            SceneManager.LoadScene(nextSceneIndex);
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                Debug.Log(timer);
+            }
+            else
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+                
+            
 
+           
         }
     }
    
